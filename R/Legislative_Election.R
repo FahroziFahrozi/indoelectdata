4714520b4fa20dfc84790749c96fac2849ae5121
FahroziFahrozi <ozifahrozi2@gmail.com>
2019-07-04 21:12:45 +0200

    documentation the datasets

---
+++ R/Legislative_Election.R
@@ -0,0 +1,14 @@
+#' Legislative_Election
+#'
+#' Datasets used to calculated the party that won the legislative election, there are three variables which used to calculated the party that won.
+#' @format a data frame with three variables
+#' \describe{
+#' \item{Names}{Names}
+#' \item{PartyNm}{Party}
+#' \item{Electorate}{Electorate in Indonesia}}
+#'
+#' @source \url{http://www.tribunnews.com/pemilu-2014/2014/05/15/daftar-caleg-yang-lolos-ke-dpr-ri}
+#'   downloaded from the source url on 28 May 2019
+#'  
+"Legislative_Election"
+
---
+++ R/totalvotes.R
@@ -0,0 +1,17 @@
+#' totalvotes
+#'
+#' Datasets used to calculated the party receiving the most votes in the legislative election, which candidate did the best and how did each electorate vote in each province.
+#' @format a data frame with five variables
+#' \describe{
+#' \item{Names}{Names}
+#' \item{PartyNm}{Party}
+#' \item{Electorate}{Electorate in Indonesia}
+#' \item{Provinces}{provinces in Indonesia}
+#' \item{Votes}{Total votes, in numeric}
+#' }
+#'@source \url{http://www.tribunnews.com/pemilu-2014/2014/05/15/daftar-caleg-yang-lolos-ke-dpr-ri}
+#' 
+#'   downloaded from the source url on 28 May 2019
+#'  
+"totalvotes"
+
